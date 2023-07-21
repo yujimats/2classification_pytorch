88915a5083f996d078b8db36f991f6fc9d5cc1ef
yujimats <chanmatsu812@gmail.com>
2023-07-21 16:20:12 +0900

    add MyDataset_path class(#3)

--- utils.py
+++ utils.py
@@ -42,3 +42,22 @@ class MyDataset(data.Dataset):
         label_type = self.list_file[index][2]
         return image_transformed, label_class
 
+class MyDataset_path(data.Dataset):
+    def __init__(self, list_file, path_input, transform=None, phase='train'):
+        self.list_file = list_file
+        self.path_input = path_input
+        self.transform = transform
+        self.phase = phase
+
+    def __len__(self):
+        # ファイル数を返す
+        return len(self.list_file)
+
+    def __getitem__(self, index):
+        # 画像のパスを取得
+        path_image = os.path.join(self.path_input, self.list_file[index][0])
+
+        # ラベルを取得
+        label_class = self.list_file[index][1]
+        label_type = self.list_file[index][2]
+        return path_image, label_class
