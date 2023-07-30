37e5f586b93c5714fcc8399667c5d3fcf8a20c25
yujimats <chanmatsu812@gmail.com>
2023-07-30 14:28:01 +0900

    add grid (#10)

--- view_score_vs_itr.py
+++ view_score_vs_itr.py
@@ -12,24 +12,28 @@ def save_score_vs_itr(path_output):
         ## loss
         ax1 = fig.add_subplot(2, 2, 1)
         ax1.plot(df['iteration'], df['loss'])
+        ax1.grid(axis='both')
         ax1.set_yscale('log')
         ax1.set_xlabel('iteration')
         ax1.set_ylabel('loss')
         ## acc
         ax1 = fig.add_subplot(2, 2, 2)
         ax1.plot(df['iteration'], df['accuracy'])
+        ax1.grid(axis='both')
         ax1.set_ylim(0, 1)
         ax1.set_xlabel('iteration')
         ax1.set_ylabel('accuracy')
         ## precision
         ax1 = fig.add_subplot(2, 2, 3)
         ax1.plot(df['iteration'], df['precision'])
+        ax1.grid(axis='both')
         ax1.set_ylim(0, 1)
         ax1.set_xlabel('iteration')
         ax1.set_ylabel('precision')
         ## precision
         ax1 = fig.add_subplot(2, 2, 4)
         ax1.plot(df['iteration'], df['recall'])
+        ax1.grid(axis='both')
         ax1.set_ylim(0, 1)
         ax1.set_xlabel('iteration')
         ax1.set_ylabel('recall')
@@ -37,11 +41,6 @@ def save_score_vs_itr(path_output):
         # save and release memory
         plt.savefig(os.path.join(path_output, f'score_vs_itr_{phase}.png'))
         plt.close()
-
-    # train
-    path_log_file = os.path.join(path_output, 'log_score_train.csv')
-
-    return
 
 if __name__=='__main__':
     save_score_vs_itr(path_output=os.path.join('output', 'itr200_VGG16pre:True_dataset_toyota_cars'))
